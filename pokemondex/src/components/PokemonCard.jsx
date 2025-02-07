8d824497caf15db8847a4096691dcff7e9c4eb9b
mbdyjk@gmail.com <mbdyjk@gmail.com>
2025-02-07 20:08:12 +0900

    style: PokemonList, PokemonCard 컴포넌트 스타일링

--- pokemondex/src/components/PokemonCard.jsx
+++ pokemondex/src/components/PokemonCard.jsx
@@ -1,7 +1,45 @@
 import React from "react";
+import styled from "styled-components";
 
-const PokemonCard = () => {
-  return <div>PokemonCard</div>;
+const Card = styled.div`
+  border: 1px solid lightgray;
+  border-radius: 10px;
+  text-align: center;
+  padding: 10px;
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+`;
+
+const Image = styled.img`
+  width: 100px;
+  height: 100px;
+  object-fit: contain;
+`;
+
+const Name = styled.p`
+  font-size: 16px;
+  margin: 10px 0;
+`;
+
+const Button = styled.button`
+  background-color: #ff0000;
+  color: #fff;
+  border: none;
+  border-radius: 5px;
+  padding: 5px 10px;
+  cursor: pointer;
+  &:hover {
+    background-color: #cc0000;
+  }
+`;
+
+const PokemonCard = ({ pokemon }) => {
+  return (
+    <Card>
+      <Image src={pokemon.image} alt={pokemon.name} />
+      <Name>{pokemon.name}</Name>
+      <Button>추가</Button>
+    </Card>
+  );
 };
 
 export default PokemonCard;
--- pokemondex/src/components/PokemonList.jsx
+++ pokemondex/src/components/PokemonList.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import styled from "styled-components";
+import PokemonCard from "./PokemonCard";
+
+const pokemonData = [];
+
+const List = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(150px, 1fr));
+  gap: 20px;
+`;
 
 const PokemonList = () => {
-  return <div></div>;
+  return (
+    <List>
+      {pokemonData.map((data) => (
+        <PokemonCard key={data.id} pokemon={data} />
+      ))}
+    </List>
+  );
 };
 
 export default PokemonList;
